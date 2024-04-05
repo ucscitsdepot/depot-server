5a6c689df4e8dbdbcf76d6f8b9b8a0274af07e69
CedricttChartier <43191516+requiem85@users.noreply.github.com>
2024-04-04 19:24:23 -0700

    testing now that print is working

--- parse.py
+++ parse.py
@@ -331,10 +331,10 @@ if __name__ == "__main__":
                                 approver = field.replace("Mail Code approver: ", "")
                             elif "Shipping Address: " in field:
                                 address1 = " ".join(field.replace("Shipping Address: ", "").split()[:3])
-                                address2 = " ".join(field.replace("Shipping Address: ", "").split()[3:4])
-                            #     city = field.replace("Shipping Address: ", "").split()[4]
-                            #     state = field.replace("Shipping Address: ", "").split()[5]
-                            #     zip_code = field.replace("Shipping Address: ", "").split()[6]
+                                # address2 = " ".join(field.replace("Shipping Address: ", "").split()[3:4])
+                                city = field.replace("Shipping Address: ", "").split()[3:4]
+                                state = field.replace("Shipping Address: ", "").split()[4:5]
+                                zip_code = field.replace("Shipping Address: ", "").split()[5]
                             elif "Return: " in field:
                                 label.returnLoc = field.replace("Return: ", "")
                                 if label.returnLoc == "":
